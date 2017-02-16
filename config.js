ba8bffc9aa347508a648d8220e65f7dedaad9e6d
mSheverda <sheverda_maxym@ukr.net>
2017-02-16 00:11:16 -0800

    ewd

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ module.exports = {
     "database" : "mongodb://root:pass@ds153699.mlab.com:53699/secondhand",
     "port" : process.env.PORT || 3000,
     "serceKey" : "SerceKey"
+     "serceKey2" : "SerceKey"
 }        
